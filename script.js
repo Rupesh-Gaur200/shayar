f5b3b52cbffbffb8bff916bd887a2d7183393667
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-05-28 23:51:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const btn = document.querySelector(".btn-open");
+const form=document.querySelector(".factform");
+btn.addEventListener('click',function(){
+   if(form.classList.contains("hidden")){
+    form.classList.remove("hidden");
+    btn.textContent="close"
+   }else{
+    form.classList.add('hidden');
+    btn.textContent="share a shayari";
+   }
+})
